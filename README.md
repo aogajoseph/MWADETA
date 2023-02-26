3766f978cc714ea9c0706937aa3dab2876a79dba
aogajoseph <josephaoga@hotmail.com>
2023-02-26 15:56:31 +0300

    upolaede project files

---
+++ README.md
@@ -0,0 +1,68 @@
+# Mwanangu Deveopment Tanzania - MWADETA
+
+This is the official MWADETA website.
+
+Mwanangu Development Tanzania, MWADETA is a non-profit organization in Dar es salaam aimed at improving access to education and healthcare for children with physical handicap.
+
+Design & Build: [Synergy Apps Inc](https://onyangojoseph.github.io/synergyapps/)
+
+
+
+## Getting Started
+
+You may [Fork]( https://github.com/aogajoseph/mwadeta.git) the Repository or [Clone]( https://github.com/onyangojoseph/mwadeta.git) it.
+
+
+
+### Prerequisites
+
+- Basic HTML, CSS and JS understanding.
+- Basic source control (e.g GitHub) understanding.
+- Basic VS Code understanding or other text editors.
+
+
+
+## Deployment
+
+Live at: [mwadeta.org](https://onyangojoseph.github.io/mwadeta/)
+
+
+
+## Built With
+
+- [Bootstrap](http://getbootstrap.com)
+- [Fontawesome](https://fontawesome.com/icons)
+- [Google Fonts](https://www.google.com/fonts)
+- [Unsplash Images](https://unsplash.com)
+- [Elegant Fonts](http://www.flaticon.com)
+
+
+
+## Contribution
+
+Please reach out for contribution guidelines, details on pull request submission and our code of conduct.
+
+
+
+## Author
+
+**Joseph Aoga**. View portfolio at [josephaoga.com](https://onyangojoseph.github.io/profolio/)
+
+
+
+## License
+
+This project is licensed under a [Free Site License CC 3.0](LICENSE.md)
+
+
+
+## Acknowledgments
+
+- **Joseph Onyango** - CEO and Founder at Synergy Apps Inc.
+- Hat tip to the **Synergy Apps Inc** team.
+- **Janet Manoni** - Regional Director East Africa, Child-Help International and Founder at MWADETA.
+- **Bootstrap**
+- **Fontawesome**
+- **Google** 
+- **Unsplash**
+- **Elegant Fonts**
---
+++ assets/js/global.js
@@ -0,0 +1,69 @@
+(function($) {
+  'use strict';
+
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
+
+    $('.popover-dismiss').popover({
+      trigger: 'focus'
+    })
+	
+  });
+AOS.init();
+  $(function() {
+    if ($('#mainNav').offset().top > 100) {
+      $('#mainNav').addClass('navbar-bg-onscroll');
+    }
+
+    $(window).on('scroll', function() {
+      var navbarOffset = $('#mainNav').offset().top > 100;
+      if(navbarOffset) {
+        $('#mainNav').addClass('navbar-bg-onscroll');
+      }
+      else {
+        $('#mainNav').removeClass('navbar-bg-onscroll');
+        $('#mainNav').addClass('navbar-bg-onscroll--fade');
+      }
+    });
+  });
+
+  $(function() {
+    $('a[href*=#js-scroll-to-]:not([href=#js-scroll-to-])').on('click', function() {
+      if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - 10
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+  
+  $('.portfolio-item').isotope({
+    itemSelector: '.item',
+    layoutMode: 'fitRows'
+  });
+         
+  $('.portfolio-menu ul li').click(function(){
+    $('.portfolio-menu ul li').removeClass('active');
+    $(this).addClass('active');     	
+    var selector = $(this).attr('data-filter');
+    $('.portfolio-item').isotope({
+    filter:selector});
+    return  false;
+  });
+         
+  $(document).ready(function() {
+    var popup_btn = $('.popup-btn');
+    popup_btn.magnificPopup({
+    type : 'image',
+    gallery : {
+      enabled : true
+    }});
+  });
+		 
+}(jQuery));
